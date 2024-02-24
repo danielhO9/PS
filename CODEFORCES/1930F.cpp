2b8e9bf6977958fa8ddc7c1b42e6291716781b1d
leeyongjun604 <daniel_604@naver.com>
2024-02-24 23:15:36 +0900

    .

--- CODEFORCES/1930F.cpp
+++ CODEFORCES/1930F.cpp
@@ -1,8 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int limit, len, ans;
+
+int add(int x, int t, vector<vector<bool>>& vis) {
+	if (vis[t][x]) return ans;
+	queue<int> Q;
+	vis[t][x] = 1; Q.push(x);
+	while (!Q.empty()) {
+		int now = Q.front(); Q.pop();
+		if (vis[0][now] && vis[1][now]) ans = max(ans, now);
+		for (int i = 0; i < len; ++i) if ((now >> i) & 1) {
+			if (!vis[t][now ^ (1 << i)]) {
+				vis[t][now ^ (1 << i)] = 1;
+				Q.push(now ^ (1 << i));
+			}
+		}
+	}
+	return ans;
+}
+
 void solve() {
-	
+	int n, q; cin >> n >> q;
+	limit = 1, len = 0, ans = 0;
+	while (limit < n) {
+		limit *= 2; ++len;
+	}
+	vector<vector<bool>> vis(2, vector<bool>(limit, 0));
+	int prev = 0;
+	while (q--) {
+		int e; cin >> e;
+
+		add((prev + e) % n, 0, vis);
+		prev = add(((1 << len) - 1) ^ ((prev + e) % n), 1, vis);
+		cout << prev << ' ';
+	}
+	cout << '\n';
 }
 
 int main() {
