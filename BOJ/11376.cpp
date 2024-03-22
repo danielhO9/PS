7ff2e14fa9ada96894c31249e05e8a8a068f4dee
leeyongjun604 <daniel_604@naver.com>
2024-03-22 15:46:31 +0900

    .

--- BOJ/11376.cpp
+++ BOJ/11376.cpp
@@ -19,18 +19,33 @@ int bipartiteMatch(int& n, int& m, vector<vector<int>>& adj) {
 	vector<int> bMatch = vector<int>(m, -1);
 	int size = 0;
 	vector<bool> visited(n, false);
-	for (int start = 0; start < n; ++start) {
-		if (dfs(start, aMatch, bMatch, visited, adj)) {
-			++size;
-			for (int i = 0; i < n; ++i) visited[i] = false;
+	for (int cnt = 0; cnt < 2; ++cnt) {
+		for (int start = 0; start < n; ++start) {
+			if (dfs(start, aMatch, bMatch, visited, adj)) {
+				++size;
+				for (int i = 0; i < n; ++i) visited[i] = false;
+			}
 		}
 	}
 	return size;
 }
 
 void solve() {
-	int n, m;
+	int n, m; cin >> n >> m;
 	// 0 ~ n - 1, 0 ~ m - 1
 	vector<vector<int>> adj(n);
+	for (int i = 0; i < n; ++i) {
+		int c; cin >> c;
+		while (c--) {
+			int s; cin >> s; --s;
+			adj[i].push_back(s);
+		}
+	}
 	int ans = bipartiteMatch(n, m, adj);
+	cout << ans;
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	solve();
 }
