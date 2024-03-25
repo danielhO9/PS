ebf0016ddbe498df483bb562c72f43e24041a460
leeyongjun604 <daniel_604@naver.com>
2024-03-25 16:53:44 +0900

    .

--- BOJ/11378.cpp
+++ BOJ/11378.cpp
@@ -1,38 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int networkFlow(int source, int sink, vector<vector<int> >& adj, vector<vector<int> >& capacity, vector<vector<int>>& flow) {
+bool bfs(int source, int sink, vector<int>& level, vector<int>& work, vector<vector<int>>& adj, vector<vector<int>>& capacity, vector<vector<int>>& flow) {
+    for (auto& i: level) i = -1;
+	level[source] = 0;
+	queue<int> q;
+	q.push(source);
+	while (!q.empty()) {
+        int here = q.front(); q.pop();
+		for (auto there: adj[here]) {
+			if (level[there] == -1 && capacity[here][there] - flow[here][there] > 0) {
+				q.push(there);
+                level[there] = level[here] + 1;
+            }
+        }
+    }
+	return level[sink] != -1;
+}
+
+int dfs(int here, int sink, int amount, vector<int>& level, vector<int>& work, vector<vector<int>>& adj, vector<vector<int>>& capacity, vector<vector<int>>& flow) {
+    if (here == sink) return amount;
+ 	for (int& i = work[here]; i < adj[here].size(); ++i){
+        int there = adj[here][i];
+		if (level[there] == level[here] + 1 && capacity[here][there] - flow[here][there] > 0) {
+            int ret = dfs(there, sink, min(capacity[here][there] - flow[here][there], amount), level, work, adj, capacity, flow);
+			if (ret > 0) {
+                flow[here][there] += ret;
+                flow[there][here] -= ret;
+				return ret;
+            }
+        }
+    }
+	return 0;
+}
+
+int networkFlow(int source, int sink, vector<vector<int>>& adj, vector<vector<int>>& capacity, vector<vector<int>>& flow) {
 	const int MAX_V = adj.size();
 	int totalFlow = 0;
-	while (true) {
-		vector<int> parent(MAX_V, -1);
-		queue<int> q;
-		parent[source] = source;
-		q.push(source);
-		while (!q.empty() && parent[sink] == -1) {
-			int here = q.front(); q.pop();
-			for (int there: adj[here]) {
-				if (capacity[here][there] - flow[here][there] > 0 && parent[there] == -1) {
-					q.push(there);
-					parent[there] = here;
-				}
-			}
-		}
-		if (parent[sink] == -1) break;
-		int amount = INT32_MAX;
-		for (int p = sink; p != source; p = parent[p]) {
-			amount = min(capacity[parent[p]][p] - flow[parent[p]][p], amount);
-		}
-		for (int p = sink; p != source; p = parent[p]) {
-			flow[parent[p]][p] += amount;
-			flow[p][parent[p]] -= amount;
-		}
-		totalFlow += amount;
-	}
+	vector<int> level(MAX_V), work(MAX_V);
+    while (bfs(source, sink, level, work, adj, capacity, flow)) {
+        for (auto& i: work) i = 0;
+		while (true) {
+            int amount = dfs(source, sink, INT32_MAX, level, work, adj, capacity, flow);
+            if (amount == 0) break;
+            totalFlow += amount;
+        }
+    }
 	return totalFlow;
 }
 
-void addEdge(int s, int e, int c, vector<vector<int> >& adj, vector<vector<int> >& capacity) {
+void addEdge(int s, int e, int c, vector<vector<int>>& adj, vector<vector<int>>& capacity) {
 	adj[s].push_back(e);
 	adj[e].push_back(s);
 	capacity[s][e] = c;
