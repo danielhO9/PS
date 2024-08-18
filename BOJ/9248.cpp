0e3c95b2d955c27b81989ee25241323335fc43d3
leeyongjun604 <daniel_604@naver.com>
2024-08-19 01:07:25 +0900

    .

--- BOJ/9248.cpp
+++ BOJ/9248.cpp
@@ -1,33 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> suffixArray(const string& s) {
-	const int MAX_N = s.size();
-	vector<int> sfx(MAX_N);
-	int m = 26;
-	vector<int> cnt(max(MAX_N, m), 0), ord(MAX_N,0), pord(MAX_N,0);
-
-	for (int i = 0; i < MAX_N; ++i) ++cnt[ord[i] = s[i] - 'a'];
-	for (int i = 1; i < m; ++i) cnt[i] += cnt[i - 1];
-	for (int i = MAX_N - 1; i >= 0; --i) sfx[--cnt[ord[i]]] = i;
-	int now = 1, idx = 0;
+vector<int> getSuffixArray(string& s) {
+	int m = 255;
+	int n = s.size();
+	int t = 1;
+	vector<int> group(n + 1), sfx(n), newGroup(n + 1), nxtord(n + 1);
+	int cnt[max(m, n) + 1];
+	
+	for(int i = 0; i < n; ++i) group[i] = s[i];
+	
+	int pt = 1;
 	while (true) {
-		if (idx == MAX_N - 1) break;
-		m = idx + 1; idx = 0;
-		for (int i = MAX_N - now; i < MAX_N; ++i) pord[idx++] = i;
-		for (int i = 0; i < MAX_N; ++i) if (sfx[i] >= now) pord[idx++] = sfx[i] - now;
-		for (int i = 0; i < m; ++i) cnt[i] = 0;
-		for (int i = 0; i < MAX_N; ++i) ++cnt[ord[pord[i]]];
-		for (int i = 1; i < m; ++i) cnt[i] += cnt[i - 1];
-		for (int i = MAX_N - 1; i >= 0; --i) sfx[--cnt[ord[pord[i]]]] = pord[i];
-		
-		pord = vector<int>(ord.begin(), ord.end()); idx = 0;
-		ord[sfx[0]] = 0;
-		for (int i = 1; i < MAX_N; ++i) {
-			if (sfx[i - 1] + now >= MAX_N || sfx[i] + now >= MAX_N || pord[sfx[i - 1]] != pord[sfx[i]] || pord[sfx[i - 1] + now] != pord[sfx[i] + now]) ++idx;
-			ord[sfx[i]] = idx;
+		// nxtord: i를 group[i + t]로 정렬
+		memset(cnt, 0, sizeof(cnt));
+		for (int i = 0; i < n; ++i) ++cnt[group[min(i + t, n)]];
+		for (int i = 1; i <= n || i <= m; ++i) cnt[i] += cnt[i - 1];
+		for (int i = 0; i < n; ++i) nxtord[--cnt[group[min(i + t, n)]]] = i;
+
+		// nxtord가 큰 i부터 뒤에서 채움
+		memset(cnt, 0, sizeof(cnt));
+		for (int i = 0; i < n; ++i) ++cnt[group[i]];
+		for (int i = 1; i <= n || i <= m; ++i) cnt[i] += cnt[i - 1];
+		for (int i = n - 1; i >= 0; --i) sfx[--cnt[group[nxtord[i]]]] = nxtord[i];
+
+		if(pt == n) break;
+		pt = 1; newGroup[sfx[0]] = 1;
+		for(int i = 1; i < n; ++i){
+			if(group[sfx[i - 1]] != group[sfx[i]] || group[min(sfx[i - 1] + t, n)] != group[min(sfx[i] + t, n)]) ++pt;
+			newGroup[sfx[i]] = pt;
 		}
-		now *= 2;
+		group = vector<int>(newGroup.begin(), newGroup.end());
+		t *= 2;
 	}
 	return sfx;
 }
@@ -51,7 +55,7 @@ vector<int> getLcp(const string& s, const vector<int>& sfx) {
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0);
 	string s; cin >> s;
-	vector<int> sfx = suffixArray(s);
+	vector<int> sfx = getSuffixArray(s);
 	vector<int> lcp = getLcp(s, sfx);
 	for (int i = 0; i < s.size(); ++i) cout << sfx[i] + 1 << ' ';
 	cout << '\n';
--- SuffixArray.cpp
+++ SuffixArray.cpp
@@ -2,44 +2,48 @@
 using namespace std;
 
 // 접미사 배열의 정렬
-vector<int> suffixArray(const string& s) {
-	const int MAX_N = s.size();
-	vector<int> sfx(MAX_N);
-	int m = 26;
-	vector<int> cnt(max(MAX_N, m), 0), ord(MAX_N,0), pord(MAX_N,0);
-
-	for (int i = 0; i < MAX_N; ++i) ++cnt[ord[i] = s[i] - 'a'];
-	for (int i = 1; i < m; ++i) cnt[i] += cnt[i - 1];
-	for (int i = MAX_N - 1; i >= 0; --i) sfx[--cnt[ord[i]]] = i;
-	int now = 1, idx = 0;
+// https://gist.github.com/koosaga/44532e5dec947132ee55da0458255e05
+vector<int> getSuffixArray(string& s) {
+	int m = 255;
+	const int n = s.size();
+	int t = 1;
+	vector<int> group(n + 1), sfx(n), newGroup(n + 1), nxtord(n + 1);
+	int cnt[max(m, n) + 1];
+	
+	for(int i = 0; i < n; ++i) group[i] = s[i];
+	
+	int pt = 1;
 	while (true) {
-		if (idx == MAX_N - 1) break;
-		m = idx + 1; idx = 0;
-		for (int i = MAX_N - now; i < MAX_N; ++i) pord[idx++] = i;
-		for (int i = 0; i < MAX_N; ++i) if (sfx[i] >= now) pord[idx++] = sfx[i] - now;
-		for (int i = 0; i < m; ++i) cnt[i] = 0;
-		for (int i = 0; i < MAX_N; ++i) ++cnt[ord[pord[i]]];
-		for (int i = 1; i < m; ++i) cnt[i] += cnt[i - 1];
-		for (int i = MAX_N - 1; i >= 0; --i) sfx[--cnt[ord[pord[i]]]] = pord[i];
-		
-		pord = vector<int>(ord.begin(), ord.end()); idx = 0;
-		ord[sfx[0]] = 0;
-		for (int i = 1; i < MAX_N; ++i) {
-			if (sfx[i - 1] + now >= MAX_N || sfx[i] + now >= MAX_N || pord[sfx[i - 1]] != pord[sfx[i]] || pord[sfx[i - 1] + now] != pord[sfx[i] + now]) ++idx;
-			ord[sfx[i]] = idx;
+		// nxtord: i를 group[i + t]로 정렬
+		memset(cnt, 0, sizeof(cnt));
+		for (int i = 0; i < n; ++i) ++cnt[group[min(i + t, n)]];
+		for (int i = 1; i <= n || i <= m; ++i) cnt[i] += cnt[i - 1];
+		for (int i = 0; i < n; ++i) nxtord[--cnt[group[min(i + t, n)]]] = i;
+
+		// nxtord가 큰 i부터 뒤에서 채움
+		memset(cnt, 0, sizeof(cnt));
+		for (int i = 0; i < n; ++i) ++cnt[group[i]];
+		for (int i = 1; i <= n || i <= m; ++i) cnt[i] += cnt[i - 1];
+		for (int i = n - 1; i >= 0; --i) sfx[--cnt[group[nxtord[i]]]] = nxtord[i];
+
+		if(pt == n) break;
+		pt = 1; newGroup[sfx[0]] = 1;
+		for(int i = 1; i < n; ++i){
+			if(group[sfx[i - 1]] != group[sfx[i]] || group[min(sfx[i - 1] + t, n)] != group[min(sfx[i] + t, n)]) ++pt;
+			newGroup[sfx[i]] = pt;
 		}
-		now *= 2;
+		group = vector<int>(newGroup.begin(), newGroup.end());
+		t *= 2;
 	}
 	return sfx;
 }
 
-// i번째와 i - 1번째 sfx에서 lcp 길이
-vector<int> commomPrefix(const string& s, const vector<int>& sfx) {
-	const int MAX_N = s.size();
-	vector<int> inv(MAX_N), lcp(MAX_N);
-	for (int i = 0; i < MAX_N; ++i) inv[sfx[i]] = i;
+vector<int> getLcp(const string& s, const vector<int>& sfx) {
+	const int n = s.size();
+	vector<int> inv(n), lcp(n);
+	for (int i = 0; i < n; ++i) inv[sfx[i]] = i;
 	int h = 0;
-	for(int i = 0; i < MAX_N; ++i){
+	for(int i = 0; i < n; ++i){
 		if(inv[i]){
 			int prv = sfx[inv[i] - 1];
 			while(s[prv + h] == s[i + h]) ++h;
@@ -51,7 +55,7 @@ vector<int> commomPrefix(const string& s, const vector<int>& sfx) {
 }
 
 void solve() {
-	string S;
-	vector<int> sfx = suffixArray(S);
-	vector<int> lcp = commomPrefix(S, sfx);
+	string S; cin >> S;
+	vector<int> sfx = getSuffixArray(S);
+	vector<int> lcp = getLcp(S, sfx);
 }
