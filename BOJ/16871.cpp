185c3a0aa9ce494b4886984f69d82e28dbb9ffdc
yongjun <leeyongjun604@gmail.com>
2024-08-30 00:02:28 +0900

    .

--- BOJ/16871.cpp
+++ BOJ/16871.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int p;
+int dig[1000];
+vector<int> idx[10];
 
 void solve() {
+	cin >> p;
+	for (int i = 0; i < p; ++i) cin >> dig[i];
+	for (int i = 0; i < p; ++i) idx[dig[i]].push_back(i);
+	for (int n = 1; n < p; ++n) for (int m = 1; m < p; ++m) {
+		for (int b = 0; b = 10; ++b) {
+			
+		}
+	}
 
 }
 
