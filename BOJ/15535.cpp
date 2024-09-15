ea2f9d2f9e3f44c100124d233ddc9035458e82ee
leeyongjun604 <daniel_604@naver.com>
2024-09-15 20:42:42 +0900

    .

--- BOJ/15535.cpp
+++ BOJ/15535.cpp
@@ -8,11 +8,17 @@ struct Point {
 
 int N;
 Point pts[100000];
+ll mn[100000];
+ll mx[100000];
+ll m[100000];
 
 void solve() {
 	cin >> N;
 	for (int i = 0; i < N; ++i) cin >> pts[i].x >> pts[i].y;
-	
+	set<int> s;
+	for (int i = 0; i < N; ++i) s.insert(pts[i].x);
+	int idx = 0;
+	for (auto i: s)
 }
 
 int main() {
