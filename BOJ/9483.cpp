4dc2a7634138f02d0625eca5126e552e7060b84c
daniel-604 <daniel_604@naver.com>
2025-07-22 16:16:32 +0900

    suffix array problem, not solved

--- BOJ/9483.cpp
+++ BOJ/9483.cpp
@@ -39,9 +39,9 @@ bool radixSort(vector<int>& rk, int k) {
     return true;
 }
 
-vector<int> getSuffixArray(const string& s) {
+vector<int> getSuffixArray(const vector<int>& s) {
     const int n = s.size();
-    const int m = 255;
+    const int m = 4;
     vector<int> cnt(m);
     for (auto i: s) cnt[i] = 1;
     for (int i = 1; i < m; ++i) cnt[i] += cnt[i - 1];
@@ -56,73 +56,48 @@ vector<int> getSuffixArray(const string& s) {
     return sfx;
 }
 
-vector<ll> getLcp(const string& s, const vector<int>& sfx) {
+vector<int> getLcp(const vector<int>& s, const vector<int>& sfx) {
 	const int n = s.size();
-	vector<ll> inv(n), lcp(n);
+	vector<int> inv(n), lcp(n);
 	for (int i = 0; i < n; ++i) inv[sfx[i]] = i;
 	int h = 0;
 	for(int i = 0; i < n; ++i) if (inv[i]) {
 		int prv = sfx[inv[i] - 1];
-		while (s[prv + h] == s[i + h]) ++h;
+		while (prv + h < n && i + h < n && s[prv + h] == s[i + h]) ++h;
 		lcp[inv[i]] = h;
 		h = max(h - 1, 0);
 	}
 	return lcp;
 }
 
-struct SegmentTree {
-	vector<ll> arr;
-	vector<ll> tree;
-	const ll dValue = LLONG_MAX; // TODO
-	int sz;
+struct SparseTable {
+    vector<vector<int>> f;
 
-	ll merge(ll a, ll b) { return min(a, b); }
-	SegmentTree(int sz) : SegmentTree(vector<ll>(sz, dValue)) {}
-	SegmentTree(const vector<ll>& a) {
-		sz = a.size();
-		arr = a;
-		int h = (int)ceil(log2(sz));
-		int tree_size = (1 << (h + 1));
-		tree = vector<ll>(tree_size);
-		init(1, 0, sz - 1);
-	}
-	void init(int node, int start, int end) {
-		if (start == end) tree[node] = arr[start];
-		else {
-			int mid = (start + end) / 2;
-			init(node * 2, start, mid);
-			init(node * 2 + 1, mid + 1, end);
-			tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
-		}
-	}
-	void update(int node, int start, int end, int index, ll val) {
-		if (index < start || index > end) return;
-		if (start == end) {
-			arr[index] += val;
-			tree[node] += val;
-			return;
-		}
-		int mid = (start + end) / 2;
-		update(node * 2, start, mid, index, val);
-		update(node * 2 + 1, mid + 1, end, index, val);
-		tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
-	}
-	ll query(int node, int start, int end, int left, int right) {
-		if (left > end || right < start) return dValue;
-		if (left <= start && end <= right) return tree[node];
-		int mid = (start + end) / 2;
-		ll lsum = query(node * 2, start, mid, left, right);
-		ll rsum = query(node * 2 + 1, mid + 1, end, left, right);
-		return merge(lsum, rsum);
-	}
-	void update(int index, ll val) { update(1, 0, sz - 1, index, val); }
-	ll query(int left, int right) { return query(1, 0, sz - 1, left, right); }
+    SparseTable(const vector<int>& a) {
+        const int n = a.size();
+        int h = (int)ceil(log2(n));
+        f.resize(n);
+        for (int i = 0; i < n; ++i) f[i] = vector<int>(h, INT32_MAX);
+        for (int j = 0; j < h; ++j) {
+            for (int i = 0; i < n; ++i) {
+                if (j == 0) f[i][j] = a[i];
+                else {
+                    if (i + (1 << (j - 1)) >= n) break;
+                    f[i][j] = min(f[i][j - 1], f[i + (1 << (j - 1))][j - 1]);
+                }
+            }
+        }
+    }
+    int query(int l, int r) {
+        int len = log2(r - l + 1);
+        return min(f[l][len], f[r - (1 << len) + 1][len]);
+    }
 };
 
-string S;
-ll n;
+string SS;
+int n;
 
-ll query(SegmentTree& seg, vector<int>& inv, int i, int j, bool rev = false) {
+int query(SparseTable& seg, vector<int>& inv, int i, int j, bool rev = false) {
     if (rev) {
         i = n - 1 - i;
         j = n - 1 - j;
@@ -133,47 +108,53 @@ ll query(SegmentTree& seg, vector<int>& inv, int i, int j, bool rev = false) {
 }
 
 void solve() {
-    n = S.size();
+    n = SS.size();
+    vector<int> S(n);
+    for (int i = 0; i < n; ++i) {
+        if (SS[i] == 'A') S[i] = 0;
+        else if (SS[i] == 'T') S[i] = 1;
+        else if (SS[i] == 'G') S[i] = 2;
+        else S[i] = 3;
+    }
     vector<int> sfx = getSuffixArray(S);
-    vector<ll> lcp = getLcp(S, sfx);
+    vector<int> lcp = getLcp(S, sfx);
     vector<int> inv(n); for (int i = 0; i < n; ++i) inv[sfx[i]] = i;
-    SegmentTree seg {lcp};
+    SparseTable seg {lcp};
 
-    string rS = S;
+    auto rS = S;
     reverse(rS.begin(), rS.end());
     vector<int> rsfx = getSuffixArray(rS);
-    vector<ll> rlcp = getLcp(rS, rsfx);
+    vector<int> rlcp = getLcp(rS, rsfx);
     vector<int> rinv(n); for (int i = 0; i < n; ++i) rinv[rsfx[i]] = i;
-    SegmentTree rseg {rlcp};
+    SparseTable rseg {rlcp};
 
     ll ans = 0;
-    for (ll len = 1; len <= n; ++len) {
-        vector<pair<ll, ll>> v;
-        for (ll i = 0; i < n; i += len) v.push_back(make_pair(i, min(i + len - 1, n - 1)));
+    vector<pair<int, int>> v;
+    for (int len = 2; len * 2 <= n; ++len) {
+        v.clear();
+        for (int i = 0; i < n; i += len) v.push_back(make_pair(i, min(i + len - 1, n - 1)));
         for (int i = 1; i < v.size(); ++i) {
-            ll lcl = query(rseg, rinv, v[i].second, v[i - 1].second, true);
+            int lcl = query(rseg, rinv, v[i].second, v[i - 1].second, true);
             lcl = min(lcl, v[i].second - v[i].first + 1);
-            if (lcl == len) {
-                // cout << v[i].first << ' ' << len << '\n';
-                ++ans;
-            }
+            if (lcl == len) ++ans;
             if (i + 1 < v.size()) {
-                ll lcr = query(seg, inv, v[i].first, v[i + 1].first);
+                int lcr = query(seg, inv, v[i].first, v[i + 1].first);
                 lcr = min(lcr, v[i + 1].second - v[i + 1].first + 1);
-                ll l = len - lcl;
-                ll r = min(lcr, len - 2);
-                if (l <= r) ans += (r - l + 1);
+                if (lcl == len) --ans;
+                if (lcr == len) --ans;
+                if (len - lcl <= lcr) ans += lcr + lcl - len + 1;
             }
         }
     }
+    for (int i = 0; i < n - 1; ++i) if (S[i] == S[i + 1]) ++ans;
     cout << ans << '\n';
 }
 
 int main() {
     ios::sync_with_stdio(0); cin.tie(0);
     while (true) {
-        cin >> S;
-        if (S[0] == '0') break;
+        cin >> SS;
+        if (SS[0] == '0') break;
         solve();
     }
 }
