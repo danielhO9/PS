5216004960223e357e4fe1bae198139f264f8614
daniel-604 <daniel_604@naver.com>
2025-07-29 13:51:48 +0900

    fwht modified

--- template/math/fwht.cpp
+++ template/math/fwht.cpp
@@ -40,20 +40,6 @@ void fwht_xor(vector<ll> &a, bool inv) {
     }
 }
 
-void fwht_xor(vector<ll> &a, bool inv) {
-    ll n = a.size();
-    for(ll s = 2, h = 1; s <= n; s <<= 1, h <<= 1) {
-        for(ll l = 0; l < n; l += s) {
-            for(ll i = 0; i < h; i++) {
-                ll t = a[l + h + i];
-                a[l + h + i] = a[l + i] - t;
-                a[l + i] += t;
-                if (inv) a[l + h + i] /= 2, a[l + i] /= 2;
-            }
-        }
-    }
-}
-
 vector<ll> convolute(vector<ll>& A, vector<ll>& B) {
     vector<ll> a(A.begin(), A.end()), b(B.begin(), B.end());
 
